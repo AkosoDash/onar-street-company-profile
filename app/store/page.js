4400370aa32b97e1018172b3e49b21f8df695a99
RaflyAmartharizqi <raflyrizqi25@gmail.com>
2023-11-26 16:44:23 +0700

    Store

--- app/store/page.js
+++ app/store/page.js
@@ -1,9 +1,23 @@
 import Image from "next/image";
 import CarouselComponent from "../../components/carousel";
+import CardStoreComponent from "../../components/card_store";
+import NavbarComponent from "../../components/navbar";
 
 
 export default function Store() {
   return (
-    <CarouselComponent />
+    <>
+      <NavbarComponent />
+      <div className="container p-4">
+        <CarouselComponent />
+      </div>
+      <div className="container p-4 grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-4">
+        
+      <CardStoreComponent />
+      <CardStoreComponent />
+      <CardStoreComponent />
+      <CardStoreComponent />
+      </div>
+    </>
   );
 }
---
+++ components/card_store.js
@@ -0,0 +1,13 @@
+// import {} from "daisyui";
+const CardStoreComponent = () => {
+    return (
+      <>
+       <div className="card bg-base-100 shadow-xl">
+    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+    </div>
+      </>
+    );
+  };
+  
+  export default CardStoreComponent;
+  
