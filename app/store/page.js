459468f3a7494f39a357e9b31109d67683fa400c
AkosoDash <akosodash12@gmail.com>
2023-11-26 17:46:34 +0700

    add marketplace data

--- app/store/page.js
+++ app/store/page.js
@@ -1,22 +1,25 @@
-import Image from "next/image";
 import CarouselComponent from "../../components/carousel";
 import CardStoreComponent from "../../components/card_store";
 import NavbarComponent from "../../components/navbar";
-
+import datas from "../../datas/marketplace.json";
 
 export default function Store() {
   return (
     <>
       <NavbarComponent />
-      <div className="container p-4">
+      <div className="container p-4 mx-auto">
         <CarouselComponent />
       </div>
-      <div className="container p-4 grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-4">
-        
-      <CardStoreComponent />
-      <CardStoreComponent />
-      <CardStoreComponent />
-      <CardStoreComponent />
+      <div className="container p-4 mx-auto grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-4">
+        {datas.map((data, index) => {
+          return (
+            <CardStoreComponent
+              name={data.name}
+              url_link={data.direct_url}
+              img_link={data.image_url}
+            />
+          );
+        })}
       </div>
     </>
   );
--- components/card_store.js
+++ components/card_store.js
@@ -1,13 +1,16 @@
+import Link from "next/link";
+
 // import {} from "daisyui";
-const CardStoreComponent = () => {
-    return (
-      <>
-       <div className="card bg-base-100 shadow-xl">
-    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+const CardStoreComponent = ({ name, url_link, img_link }) => {
+  return (
+    <div className="card bg-base-100 shadow-xl p-5">
+      <figure>
+        <a href={`https://${url_link}`} target="_blank">
+          <img src={img_link} alt={name} className="bg-base-300 rounded-xl" />
+        </a>
+      </figure>
     </div>
-      </>
-    );
-  };
-  
-  export default CardStoreComponent;
-  
+  );
+};
+
+export default CardStoreComponent;
